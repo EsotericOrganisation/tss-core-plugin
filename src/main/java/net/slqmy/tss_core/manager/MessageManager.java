be3fb422eba52d70daab668c1679ee63e2218c48
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-02 19:21:15 +0100

    📄 Added message caching, placeholder support and more methods.

--- src/main/java/net/slqmy/tss_core/manager/MessageManager.java
+++ src/main/java/net/slqmy/tss_core/manager/MessageManager.java
@@ -1,23 +1,35 @@
 package net.slqmy.tss_core.manager;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.slqmy.tss_core.TSSCorePlugin;
 import net.slqmy.tss_core.data.Message;
 import net.slqmy.tss_core.data.type.Lang;
 import net.slqmy.tss_core.type.PlayerProfile;
+import net.slqmy.tss_core.util.DebugUtil;
+import net.slqmy.tss_core.util.FileUtil;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 public class MessageManager {
+
+	private final static Pattern PLACEHOLDER_PATTERN = Pattern.compile("\\{\\d+}");
 
 	private final TSSCorePlugin plugin;
 
 	private final HashMap<Lang, YamlConfiguration> langConfigs = new HashMap<>();
+
+	private final HashMap<Lang, Cache<Message, TextComponent>> messageCaches = new HashMap<>();
 
 	public MessageManager(@NotNull TSSCorePlugin plugin) {
 		this.plugin = plugin;
@@ -27,30 +39,85 @@ public class MessageManager {
 			YamlConfiguration langConfig = YamlConfiguration.loadConfiguration(langFile);
 
 			langConfigs.put(lang, langConfig);
+
+			messageCaches.put(
+							lang,
+							CacheBuilder.newBuilder()
+											.expireAfterWrite(2, TimeUnit.MINUTES)
+											.build()
+			);
 		}
 	}
 
-	public Component getPlayerMessage(Message messageKey, Player player) {
-		PlayerProfile profile = plugin.getPlayerManager().getProfile(player);
+	public TextComponent getMessage(Message messageKey, Lang lang, Component ... placeHolderValues) {
+		final TextComponent cachedMessage = messageCaches.get(lang).asMap().get(messageKey);
 
-		Lang lang;
-
-		if (profile == null || profile.getPreferences() == null || profile.getPreferences().getLang() == null) {
-			lang = Lang.DEFAULT_LANG;
-		} else {
-			lang = profile.getPreferences().getLang();
+		if (cachedMessage != null) {
+			return cachedMessage;
 		}
 
-		return LegacyComponentSerializer.legacyAmpersand().deserialize(
-						getRawMessage(messageKey, lang)
+		String rawMessage = getRawMessage(messageKey, lang);
+		String[] splitMessage = PLACEHOLDER_PATTERN.split(rawMessage, placeHolderValues.length + 1);
+
+		TextComponent finalMessage = LegacyComponentSerializer.legacyAmpersand().deserialize(splitMessage[0]);
+
+		for (int i = 0; i < placeHolderValues.length; i++) {
+			finalMessage = finalMessage.append(placeHolderValues[i]).append(LegacyComponentSerializer.legacyAmpersand().deserialize(splitMessage[i + 1]));
+		}
+
+		return finalMessage;
+	}
+
+	public TextComponent getMessage(Message messageKey, Component ... placeHolderValues) {
+		return getMessage(messageKey, Lang.DEFAULT_LANG, placeHolderValues);
+	}
+
+	public TextComponent getMessage(Message messageKey, String ... placeHolderValues) {
+		return getMessage(
+						messageKey,
+						Lang.DEFAULT_LANG,
+						Arrays.stream(placeHolderValues).map((String placeHolderValue) -> LegacyComponentSerializer.legacyAmpersand().deserialize(placeHolderValue)).toArray(TextComponent[]::new)
 		);
 	}
 
-	public String getRawMessage(@NotNull Message messageKey, Lang lang) {
+	public TextComponent getMessage(Message messageKey) {
+		return getMessage(
+						messageKey,
+						Lang.DEFAULT_LANG
+		);
+	}
+
+	public TextComponent getPlayerMessage(Message messageKey, Player player, Component... placeHolderValues) {
+		PlayerProfile profile = plugin.getPlayerManager().getProfile(player);
+
+		Lang lang = profile == null ||
+						profile.getPreferences() == null ||
+						profile.getPreferences().getLang() == null
+						? Lang.DEFAULT_LANG
+						: profile.getPreferences().getLang();
+
+		return getMessage(messageKey, lang, placeHolderValues);
+	}
+
+	public TextComponent getPlayerMessage(Message messageKey, Player player, String... placeHolderValues) {
+		return getPlayerMessage(
+						messageKey,
+						player,
+						Arrays.stream(placeHolderValues).map((String placeHolderValue) -> LegacyComponentSerializer.legacyAmpersand().deserialize(placeHolderValue)).toArray(TextComponent[]::new)
+		);
+	}
+
+	public TextComponent getPlayerMessage(Message messageKey, Player player) {
+		return getPlayerMessage(messageKey, player, new Component[]{});
+	}
+
+	private String getRawMessage(@NotNull Message messageKey, Lang lang) {
+		DebugUtil.log(langConfigs, langConfigs.get(lang), langConfigs.get(lang).getString(messageKey.getKey()), messageKey, messageKey.getKey());
+
 		return langConfigs.get(lang).getString(messageKey.getKey());
 	}
 
-	public String getRawMessage(@NotNull Message messageKey) {
+	private String getRawMessage(@NotNull Message messageKey) {
 		return getRawMessage(messageKey, Lang.DEFAULT_LANG);
 	}
 }
