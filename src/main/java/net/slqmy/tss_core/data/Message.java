245a4b1bef81648260d3c813cafc940d41da485b
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-02 19:21:31 +0100

    📄 Added more messages.

--- src/main/java/net/slqmy/tss_core/data/Message.java
+++ src/main/java/net/slqmy/tss_core/data/Message.java
@@ -1,9 +1,14 @@
 package net.slqmy.tss_core.data;
 
 public enum Message {
-	UNABLE_TO_LOAD_DATA;
+	UNABLE_TO_LOAD_DATA,
+	NOT_PLAYER_COMMAND,
+	NOT_CONSOLE_COMMAND,
+	NOT_COMMAND_BLOCK_COMMAND,
+	INVALID_COMMAND_USAGE,
+	CLICK_TO_JOIN;
 
-	private String key;
+	private final String key;
 
 	Message() {
 		key = name().toLowerCase().replace('_', '-');
