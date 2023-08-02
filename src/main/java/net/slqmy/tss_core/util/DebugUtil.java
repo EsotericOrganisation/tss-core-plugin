890198b48ce2ceab9fea36fe7eeef9dcfda25ed2
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-02 19:20:24 +0100

    📋 Reworked logging and debugging.

--- src/main/java/net/slqmy/tss_core/util/DebugUtil.java
+++ src/main/java/net/slqmy/tss_core/util/DebugUtil.java
@@ -1,21 +1,43 @@
 package net.slqmy.tss_core.util;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.jetbrains.annotations.NotNull;
 
 public class DebugUtil {
-	private static final String DEBUG_PREFIX = "[Debug]";
+
+	private static final Component DEBUG_PREFIX = LegacyComponentSerializer.legacyAmpersand().deserialize("&e[Debug]");
+	private final static TextComponent ERROR_PREFIX = LegacyComponentSerializer.legacyAmpersand().deserialize("[&cError&r]");
 
 	public static void log(Object @NotNull ... values) {
 		for (Object value : values) {
-			LogUtil.log(DEBUG_PREFIX + " " + value);
+			LogUtil.log(
+							DEBUG_PREFIX.append(Component.text(" ")).append(MessageUtil.format(value))
+			);
 		}
 	}
 
-	public static void handleException(@NotNull Exception exception, String message) {
-		LogUtil.error(message);
-		LogUtil.error(exception.getMessage());
-		LogUtil.error(exception);
+	public static void error(@NotNull Object error) {
+		LogUtil.log(
+						ERROR_PREFIX
+										.append(Component.text(" "))
+										.append(MessageUtil.format(error))
+		);
+	}
+
+	public static void handleException(String message, @NotNull Exception exception) {
+		if (message != null) {
+			error(LegacyComponentSerializer.legacyAmpersand().deserialize(message));
+		}
+
+		error(exception.getMessage());
+		error(exception);
 
 		exception.printStackTrace();
+	}
+
+	public static void handleException(@NotNull Exception exception) {
+		handleException(null, exception);
 	}
 }
--- src/main/java/net/slqmy/tss_core/util/LogUtil.java
+++ src/main/java/net/slqmy/tss_core/util/LogUtil.java
@@ -1,20 +1,22 @@
 package net.slqmy.tss_core.util;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.logging.Logger;
+import org.bukkit.command.ConsoleCommandSender;
 
 public class LogUtil {
-	private static final Logger LOGGER = Bukkit.getLogger();
 
-	public static void log(String message) {
-		LOGGER.info(message);
+	private final static ConsoleCommandSender CONSOLE = Bukkit.getConsoleSender();
+
+	private final static TextComponent LOG_PREFIX = LegacyComponentSerializer.legacyAmpersand().deserialize("[&a&lTSS&r-&a&lCore&r]");
+
+	public static void log(Component message) {
+		CONSOLE.sendMessage(LOG_PREFIX.append(Component.text(" ").append(message)));
 	}
 
-	public static void error(@NotNull Object message) {
-		String finalMessage = message.toString();
-
-		LOGGER.severe(finalMessage);
+	public static void log(String message) {
+		log(LegacyComponentSerializer.legacyAmpersand().deserialize(message));
 	}
 }
