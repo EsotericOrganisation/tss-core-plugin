49b0ad2062f28729f9d08f7d2476e863aae9d58f
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-04 20:28:44 +0100

    📄 Improved caching and added support for fields in super classes. (need same for methods)

--- src/main/java/net/slqmy/tss_core/util/ReflectUtil.java
+++ src/main/java/net/slqmy/tss_core/util/ReflectUtil.java
@@ -23,22 +23,21 @@ public class ReflectUtil {
 
 	public static @Nullable Class<?> getClassByName(String className) {
 		ConcurrentMap<String, Class<?>> classMap = classCache.asMap();
-
 		Class<?> targetClass = classMap.get(className);
 
-		if (targetClass != null) {
-			return targetClass;
+		if (targetClass == null) {
+			try {
+				targetClass = Class.forName(className);
+			} catch (ClassNotFoundException exception) {
+				DebugUtil.handleException("Reflection Failure: Class by the name of '" + className + "' does not exist!", exception);
+				return null;
+			}
+		} else {
+			classCache.invalidate(className);
 		}
 
-		try {
-			targetClass = Class.forName(className);
-
-			classMap.put(className, targetClass);
-		} catch (ClassNotFoundException exception) {
-			DebugUtil.handleException("Reflection Failure: Class by the name of '" + className + "' does not exist!", exception);
-		}
-
-		return null;
+		classMap.put(className, targetClass);
+		return targetClass;
 	}
 
 	public static @Nullable Object getStaticFieldValue(Class<?> targetClass, String fieldName) {
@@ -49,9 +48,9 @@ public class ReflectUtil {
 				return field.get(null);
 			}
 
-			DebugUtil.error("Reflection Failure: The static field '" + fieldName + "' of class " + targetClass.getName() + " does not exist!");
+			DebugUtil.error("Reflection Failure: The static field '" + fieldName + "' of '" + targetClass.getName() + "' does not exist!");
 		} catch (IllegalAccessException exception) {
-			DebugUtil.handleException("Reflection Failure: An unexpected exception occurred while retrieving the value of static field '" + fieldName + "' of class '" + targetClass.getName() + "'!", exception);
+			DebugUtil.handleException("Reflection Failure: An unexpected exception occurred while retrieving the value of static field '" + fieldName + "' of '" + targetClass + "'!", exception);
 		}
 
 		return null;
@@ -61,23 +60,49 @@ public class ReflectUtil {
 		return getStaticFieldValue(object.getClass(), fieldName);
 	}
 
-	public static @Nullable Object getFieldValue(@NotNull Object object, String fieldName) {
+	private static @Nullable Object getFieldValue(Class<?> targetClass, @NotNull Object object, String fieldName, Class<?> expectedFieldType) {
 		try {
-			Field field = getAccessibleField(object, fieldName);
+			Field field = getAccessibleField(targetClass, fieldName, expectedFieldType);
 
 			if (field != null) {
 				return field.get(object);
 			}
 
-			DebugUtil.error("Reflection Failure: Non-static field '" + fieldName + "' of object '" + object + "' does not exist! (Class: '" + object.getClass().getName() + "')");
+			DebugUtil.error("Reflection Failure: Non-static field '" + fieldName + "' of object '" + object + "' does not exist! (" + object.getClass() + ")");
 		} catch (IllegalAccessException exception) {
-			DebugUtil.handleException("Reflection Failure: An unexpected exception occurred while retrieving the value of non-static field '" + fieldName + "' of object '" + object + "'! (Class: '" + object.getClass().getName() + "')", exception);
+			DebugUtil.handleException("Reflection Failure: An unexpected exception occurred while retrieving the value of non-static field '" + fieldName + "' of object '" + object + "'! (" + object.getClass() + ")", exception);
 		}
 
 		return null;
 	}
 
-	private static @Nullable Field getAccessibleField(@NotNull Class<?> targetClass, String fieldName) {
+	public static @Nullable Object getFieldValue(@NotNull Object object, String fieldName, Class<?> expectedFieldType) {
+
+		DebugUtil.log("Called getFieldValue.");
+		DebugUtil.log("Object to get the value of: " + object);
+		DebugUtil.log("Name of the field to get: " + fieldName);
+		DebugUtil.log("Expected field type: " + expectedFieldType);
+
+		return getFieldValue(object.getClass(), object, fieldName, expectedFieldType);
+	}
+
+	public static @Nullable Object getFieldValue(@NotNull Object object, String fieldName) {
+		return getFieldValue(object.getClass(), object, fieldName, null);
+	}
+
+	private static @Nullable Field getAccessibleField(Class<?> targetClass, Class<?> originalClass, String fieldName, Class<?> expectedFieldType) {
+
+		DebugUtil.log("Called main getAccessibleField method.");
+		DebugUtil.log("targetClass: " + targetClass);
+		DebugUtil.log("originalClass: " + originalClass);
+		DebugUtil.log("expectedFieldType: " + expectedFieldType);
+
+		if (targetClass == null) {
+			DebugUtil.error("Reflection Failure: Couldn't find field '" + fieldName + "' as argument 'targetClass' was &9null&c!");
+			DebugUtil.log("Original target class: " + originalClass);
+			return null;
+		}
+
 		Cache<String, Field> classFieldCache = classFieldCaches.get(targetClass);
 
 		if (classFieldCache == null) {
@@ -92,30 +117,60 @@ public class ReflectUtil {
 		Map<String, Field> classFieldMap = classFieldCache.asMap();
 		Field field = classFieldMap.get(fieldName);
 
-		if (field != null) {
-			return field;
+		DebugUtil.log("Cached field: ", field);
+
+		if (field == null || (expectedFieldType != null && !expectedFieldType.equals(field.getType()))) {
+			try {
+				field = targetClass.getDeclaredField(fieldName);
+
+				DebugUtil.log("Found field " + field + ".");
+				DebugUtil.log("Is the field type as expected?", field.getType().equals(expectedFieldType));
+
+				if (expectedFieldType != null && !field.getType().equals(expectedFieldType)) {
+					DebugUtil.log("The field type is not as expected, moving on to searching super class.");
+
+					throw new NoSuchFieldException();
+				}
+
+				field.setAccessible(true);
+
+				classFieldCaches.get(originalClass).put(fieldName, field);
+			} catch (NoSuchFieldException exception) {
+				DebugUtil.log("NoSuchFieldException created, moving on to super class.");
+
+				field = getAccessibleField(targetClass.getSuperclass(), originalClass, fieldName, expectedFieldType);
+			}
+		} else {
+			classFieldCache.invalidate(fieldName);
 		}
 
-		try {
-			field = targetClass.getDeclaredField(fieldName);
+		classFieldMap.put(fieldName, field);
+		return field;
+	}
 
-			field.setAccessible(true);
+	private static @Nullable Field getAccessibleField(Class<?> targetClass, String fieldName, Class<?> expectedFieldType) {
+		return getAccessibleField(targetClass, targetClass, fieldName, expectedFieldType);
+	}
 
-			classFieldMap.put(fieldName, field);
+	private static @Nullable Field getAccessibleField(@NotNull Object object, String fieldName, Class<?> expectedFieldType) {
+		return getAccessibleField(object.getClass(), object.getClass(), fieldName, expectedFieldType);
+	}
 
-			return field;
-		} catch (NoSuchFieldException exception) {
-			DebugUtil.handleException("Reflection Failure: An unexpected exception occurred setting access of field '" + fieldName + "' of class '" + targetClass.getName() + "' to &atrue&c!", exception);
-		}
-
-		return null;
+	private static @Nullable Field getAccessibleField(Class<?> targetClass, String fieldName) {
+		return getAccessibleField(targetClass, targetClass, fieldName, null);
 	}
 
 	private static @Nullable Field getAccessibleField(@NotNull Object object, String fieldName) {
-		return getAccessibleField(object.getClass(), fieldName);
+		return getAccessibleField(object.getClass(), object.getClass(), fieldName, null);
 	}
 
-	public static @Nullable Method getAccessibleMethod(@NotNull Class<?> targetClass, String methodName) {
+	public static @Nullable Method getAccessibleMethod(Class<?> targetClass, Class<?> originalClass, String methodName) {
+		if (targetClass == null) {
+			DebugUtil.error("Reflection Failure: Couldn't find method '" + methodName + "' as argument 'targetClass' was &9null&c!");
+			DebugUtil.log("Original target class: " + originalClass);
+			return null;
+		}
+
 		Cache<String, Method> classMethodCache = classMethodCaches.get(targetClass);
 
 		if (classMethodCache == null) {
@@ -130,26 +185,28 @@ public class ReflectUtil {
 		Map<String, Method> classMethodMap = classMethodCache.asMap();
 		Method method = classMethodMap.get(methodName);
 
-		if (method != null) {
-			return method;
+		if (method == null) {
+			try {
+				method = targetClass.getDeclaredMethod(methodName);
+				method.setAccessible(true);
+
+				classMethodCaches.get(originalClass).put(methodName, method);
+			} catch (NoSuchMethodException exception) {
+				method = getAccessibleMethod(targetClass.getSuperclass(), originalClass, methodName);
+			}
+		} else {
+			classMethodCache.invalidate(methodName);
 		}
 
-		try {
-			method = targetClass.getDeclaredMethod(methodName);
+		classMethodMap.put(methodName, method);
+		return method;
+	}
 
-			method.setAccessible(true);
-
-			classMethodMap.put(methodName, method);
-
-			return method;
-		} catch (NoSuchMethodException exception) {
-			DebugUtil.handleException("Reflection Failure: An unexpected exception occurred setting access of method '" + methodName + "' of class '" + targetClass.getName() + "' to &atrue&c!", exception);
-		}
-
-		return null;
+	public static @Nullable Method getAccessibleMethod(Class<?> targetClass, String methodName) {
+		return getAccessibleMethod(targetClass, targetClass, methodName);
 	}
 
 	public static @Nullable Method getAccessibleMethod(@NotNull Object object, String methodName) {
-		return getAccessibleMethod(object.getClass(), methodName);
+		return getAccessibleMethod(object.getClass(), object.getClass(), methodName);
 	}
 }
