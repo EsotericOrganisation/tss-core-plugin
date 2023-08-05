e19d4996e14018c2c42c797ecdd3c6fed2186780
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-05 02:03:12 +0100

    📋 Added some utility methods.

--- src/main/java/net/slqmy/tss_core/util/MessageUtil.java
+++ src/main/java/net/slqmy/tss_core/util/MessageUtil.java
@@ -4,8 +4,15 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.slqmy.tss_core.type.Colour;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class MessageUtil {
+
+	private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.#");
+
 	public static TextComponent format(Object value) {
 		return value == null
 						? Component.text("null", Colour.PINK.asTextColour())
@@ -17,5 +24,18 @@ public class MessageUtil {
 						: value instanceof String
 						? LegacyComponentSerializer.legacyAmpersand().deserialize((String) value)
 						: Component.text(value.toString());
+	}
+
+	public static @NotNull TextComponent getPipeTextComponent() {
+		return Component.text("| ", Colour.GREY.asTextColour());
+	}
+
+	public static @NotNull TextComponent getColonTextComponent() {
+		return Component.text(": ", Colour.LIGHT_GREY.asTextColour());
+	}
+
+	public static @NotNull String formatNumber(double number) {
+		DECIMAL_FORMAT.setRoundingMode(RoundingMode.HALF_UP);
+		return DECIMAL_FORMAT.format(number);
 	}
 }
