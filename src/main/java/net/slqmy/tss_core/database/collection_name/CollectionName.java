4ddedde6507f555f7b26b528996aa813c1846c72
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-05 16:24:03 +0100

    📋 Added a `CollectionName` interface to generalise collection name enums.

---
+++ src/main/java/net/slqmy/tss_core/database/collection_name/CollectionName.java
@@ -0,0 +1,9 @@
+package net.slqmy.tss_core.database.collection_name;
+
+import net.slqmy.tss_core.database.DatabaseName;
+
+public interface CollectionName {
+	String getName();
+
+	DatabaseName getDatabase();
+}
--- src/main/java/net/slqmy/tss_core/database/collection_name/PlayersCollectionName.java
+++ src/main/java/net/slqmy/tss_core/database/collection_name/PlayersCollectionName.java
@@ -1,15 +1,24 @@
 package net.slqmy.tss_core.database.collection_name;
 
-public enum PlayersCollectionName {
+import net.slqmy.tss_core.database.DatabaseName;
+
+public enum PlayersCollectionName implements CollectionName {
 	PLAYER_PROFILES;
 
+	private final static DatabaseName database = DatabaseName.PLAYERS;
 	private final String name;
 
 	PlayersCollectionName() {
 		name = name().toLowerCase();
 	}
 
+	@Override
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public DatabaseName getDatabase() {
+		return database;
 	}
 }
