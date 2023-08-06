136eceb2d328dcedb6059ec861a3f80e94b71e64
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-06 21:34:14 +0100

    📋 Created listener for NPC clicks. (broken due to ReflectUtil)

--- src/main/java/net/slqmy/tss_core/manager/PacketManager.java
+++ src/main/java/net/slqmy/tss_core/manager/PacketManager.java
@@ -2,32 +2,44 @@ package net.slqmy.tss_core.manager;
 
 import io.netty.channel.*;
 import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.slqmy.tss_core.TSSCorePlugin;
+import net.slqmy.tss_core.event.custom_event.NPCClickEvent;
+import net.slqmy.tss_core.type.NPCPlayer;
+import net.slqmy.tss_core.util.DebugUtil;
 import net.slqmy.tss_core.util.NMSUtil;
 import net.slqmy.tss_core.util.ReflectUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class PacketManager {
 
-	private final ChannelDuplexHandler handler;
+	private final TSSCorePlugin plugin;
 
-	public PacketManager() {
-		handler = new ChannelDuplexHandler() {
-			@Override
-			public void read(ChannelHandlerContext context) throws Exception {
-				super.read(context);
-			}
-
-			@Override
-			public void write(ChannelHandlerContext context, Object message, ChannelPromise promise) throws Exception {
-				super.write(context, message, promise);
-			}
-		};
+	public PacketManager(TSSCorePlugin plugin) {
+		this.plugin = plugin;
 	}
 
 	public void injectPlayer(@NotNull Player player) {
+		ChannelDuplexHandler handler = new ChannelDuplexHandler() {
+			@Override
+			public void channelRead(@NotNull ChannelHandlerContext context, @NotNull Object packet) throws Exception {
+				if (packet instanceof ServerboundInteractPacket) {
+					handleServerboundInteractPacket((ServerboundInteractPacket) packet, player);
+				}
+
+				super.channelRead(context, packet);
+			}
+
+			@Override
+			public void write(ChannelHandlerContext context, Object packet, ChannelPromise promise) throws Exception {
+				super.write(context, packet, promise);
+			}
+		};
+
 		Connection connection = getPlayerConnection(player);
 		ChannelPipeline pipeline = connection.channel.pipeline();
 
@@ -60,5 +72,37 @@ public class PacketManager {
 						serverPlayerConnection,
 						"h"
 		);
+	}
+
+	private void handleServerboundInteractPacket(@NotNull ServerboundInteractPacket packet, Player player) {
+		DebugUtil.log("Handing server bound interact packet.");
+
+		int entityID = packet.getEntityId();
+		ServerboundInteractPacket.ActionType actionType = packet.getActionType();
+
+		DebugUtil.log(actionType, actionType == ServerboundInteractPacket.ActionType.INTERACT_AT);
+
+		if (actionType == ServerboundInteractPacket.ActionType.INTERACT_AT) {
+			DebugUtil.log("Wrong action type.");
+			return;
+		}
+
+		Object hand = ReflectUtil.getFieldValue(actionType, "a", false);
+		DebugUtil.log(hand, hand != null ? hand.getClass() + " " + hand : "null²");
+
+		if (hand != null && !hand.toString().equals("MAIN_HAND")) {
+			DebugUtil.log("Hand either null or offhand.");
+			return;
+		}
+
+		DebugUtil.log("1.");
+
+		NPCPlayer npcPlayer = plugin.getNpcManager().getNpcs().get(entityID);
+
+		DebugUtil.log(npcPlayer);
+
+		if (npcPlayer != null) {
+			Bukkit.getPluginManager().callEvent(new NPCClickEvent(npcPlayer, player));
+		}
 	}
 }
