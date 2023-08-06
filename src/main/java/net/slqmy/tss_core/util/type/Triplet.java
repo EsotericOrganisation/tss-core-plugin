a5ad480e34739987e4fd0fcb40f9ab678df152c4
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-06 21:20:18 +0100

    📋 Created pair and triplet utility classes.

---
+++ src/main/java/net/slqmy/tss_core/util/type/Triplet.java
@@ -0,0 +1,38 @@
+package net.slqmy.tss_core.util.type;
+
+public class Triplet<A, B, C> {
+
+	private A first;
+	private B second;
+	private C third;
+
+	public Triplet(A first, B second, C third) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+	}
+
+	public A getFirst() {
+		return first;
+	}
+
+	public void setFirst(A first) {
+		this.first = first;
+	}
+
+	public B getSecond() {
+		return second;
+	}
+
+	public void setSecond(B second) {
+		this.second = second;
+	}
+
+	public C getThird() {
+		return third;
+	}
+
+	public void setThird(C third) {
+		this.third = third;
+	}
+}
