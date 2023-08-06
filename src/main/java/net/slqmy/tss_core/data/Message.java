820d2a1729e3395bebbf1e434edb46b0f048c9e5
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-06 16:55:59 +0100

    📄 Converted messages to use MiniMessage format and added more messages.

--- src/main/java/net/slqmy/tss_core/data/Message.java
+++ src/main/java/net/slqmy/tss_core/data/Message.java
@@ -1,8 +1,15 @@
 package net.slqmy.tss_core.data;
 
 public enum Message {
+	// - The Slimy Swamp Messages
+
+	// General
+
+	THE_SLIMY_SWAMP,
+
 	// TSS-Core
 	UNABLE_TO_LOAD_DATA,
+
 	// TSS-SlimeLabs
 	ACTIVE_LAUNCH_ANALYSIS_TASK,
 	LAUNCH,
@@ -13,7 +20,20 @@ public enum Message {
 	ON_GROUND,
 
 	NO_ANALYSIS_TASK,
-	LAUNCH_STATISTICS;
+	LAUNCH_STATISTICS,
+
+	// TSS-Lobby
+
+	WELCOME_SUBTITLE,
+	WELCOME_CHAT_MESSAGE,
+
+	// - Errors and In-Plugin Messages
+
+	// TSS-Core
+	SUCCESSFUL_DATABASE_CONNECTION,
+	DATABASE_CONNECTION_FAILED,
+
+	ERROR_LOADING_PLAYER_PROFILE;
 
 	private final String key;
 
