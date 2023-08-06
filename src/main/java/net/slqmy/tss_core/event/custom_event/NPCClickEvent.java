86cc5229467e0123a8a8cf526db3b9f624b6bfbf
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-06 21:18:06 +0100

    🔔 Created event for NPC clicks.

---
+++ src/main/java/net/slqmy/tss_core/event/custom_event/NPCClickEvent.java
@@ -0,0 +1,38 @@
+package net.slqmy.tss_core.event.custom_event;
+
+import net.slqmy.tss_core.type.NPCPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class NPCClickEvent extends Event {
+
+	private static final HandlerList handlerList = new HandlerList();
+
+	private final NPCPlayer npcPlayer;
+	private final Player player;
+
+
+	public NPCClickEvent(NPCPlayer npcPlayer, Player player) {
+		this.npcPlayer = npcPlayer;
+		this.player = player;
+	}
+
+	public NPCPlayer getNpcPlayer() {
+		return npcPlayer;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public HandlerList getHandlerList() {
+		return handlerList;
+	}
+
+	@Override
+	public @NotNull HandlerList getHandlers() {
+		return handlerList;
+	}
+}
