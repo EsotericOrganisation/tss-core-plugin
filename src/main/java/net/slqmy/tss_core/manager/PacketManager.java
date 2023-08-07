c1bfdbef9a0240861e66ac281f9c04c496fd12b6
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-07 18:00:02 +0100

    📄 Finally fixed packet manager! NPCClickEvent is not working fine.

--- src/main/java/net/slqmy/tss_core/manager/PacketManager.java
+++ src/main/java/net/slqmy/tss_core/manager/PacketManager.java
@@ -5,10 +5,10 @@ import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.InteractionHand;
 import net.slqmy.tss_core.TSSCorePlugin;
 import net.slqmy.tss_core.event.custom_event.NPCClickEvent;
 import net.slqmy.tss_core.type.NPCPlayer;
-import net.slqmy.tss_core.util.DebugUtil;
 import net.slqmy.tss_core.util.NMSUtil;
 import net.slqmy.tss_core.util.ReflectUtil;
 import org.bukkit.Bukkit;
@@ -75,31 +75,23 @@ public class PacketManager {
 	}
 
 	private void handleServerboundInteractPacket(@NotNull ServerboundInteractPacket packet, Player player) {
-		DebugUtil.log("Handing server bound interact packet.");
-
-		int entityID = packet.getEntityId();
+		Integer entityID = packet.getEntityId();
 		ServerboundInteractPacket.ActionType actionType = packet.getActionType();
 
-		DebugUtil.log(actionType, actionType == ServerboundInteractPacket.ActionType.INTERACT_AT);
-
 		if (actionType == ServerboundInteractPacket.ActionType.INTERACT_AT) {
-			DebugUtil.log("Wrong action type.");
 			return;
 		}
 
-		Object hand = ReflectUtil.getFieldValue(actionType, "a", false);
-		DebugUtil.log(hand, hand != null ? hand.getClass() + " " + hand : "null²");
+		Object action = ReflectUtil.getFieldValue(packet, "b");
+		assert action != null;
 
-		if (hand != null && !hand.toString().equals("MAIN_HAND")) {
-			DebugUtil.log("Hand either null or offhand.");
+		InteractionHand hand = (InteractionHand) ReflectUtil.getFieldValue(action, "a", false);
+
+		if (hand == InteractionHand.OFF_HAND) {
 			return;
 		}
-
-		DebugUtil.log("1.");
 
 		NPCPlayer npcPlayer = plugin.getNpcManager().getNpcs().get(entityID);
-
-		DebugUtil.log(npcPlayer);
 
 		if (npcPlayer != null) {
 			Bukkit.getPluginManager().callEvent(new NPCClickEvent(npcPlayer, player));
