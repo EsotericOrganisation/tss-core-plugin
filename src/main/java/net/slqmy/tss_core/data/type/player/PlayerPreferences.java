882daaed60ee54fcd4d0420a5c206b62dcbf0967
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-09 22:48:34 +0100

    🎆🧨🎇 Added firework setting and improved firework launching.

--- src/main/java/net/slqmy/tss_core/data/type/player/PlayerPreferences.java
+++ src/main/java/net/slqmy/tss_core/data/type/player/PlayerPreferences.java
@@ -3,6 +3,7 @@ package net.slqmy.tss_core.data.type.player;
 public class PlayerPreferences {
 
 	private Language language;
+	private boolean lobbyFireworkEnabled;
 
 	public PlayerPreferences() {
 		language = Language.DEFAULT_LANGUAGE;
@@ -14,5 +15,13 @@ public class PlayerPreferences {
 
 	public void setLanguage(Language language) {
 		this.language = language;
+	}
+
+	public boolean isLobbyFireworkEnabled() {
+		return lobbyFireworkEnabled;
+	}
+
+	public void setLobbyFireworkEnabled(boolean lobbyFireworkEnabled) {
+		this.lobbyFireworkEnabled = lobbyFireworkEnabled;
 	}
 }
