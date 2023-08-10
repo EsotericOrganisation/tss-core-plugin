42500b794b7660cb5bca7e91c05e7609f08cfe07
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-10 16:51:24 +0100

    🌍 Added utility methods.

--- src/main/java/net/slqmy/tss_core/data/type/player/Language.java
+++ src/main/java/net/slqmy/tss_core/data/type/player/Language.java
@@ -1,5 +1,16 @@
 package net.slqmy.tss_core.data.type.player;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum Language {
-	EN_GB
+	EN_GB,
+	EN_US;
+
+	public static Language getLanguage(@NotNull String languageKey) {
+		return Language.valueOf(languageKey.toUpperCase().replace('-', '_'));
+	}
+
+	public @NotNull String getLanguageKey() {
+		return name().toLowerCase().replace('_', '-');
+	}
 }
