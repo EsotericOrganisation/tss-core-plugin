7ff72d319f35c9baecec9826edc9ee165d0aa111
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-19 00:29:23 +0100

    🐛 Fixed some NPEs.

--- src/main/java/net/slqmy/tss_core/event/listener/CustomGuiListener.java
+++ src/main/java/net/slqmy/tss_core/event/listener/CustomGuiListener.java
@@ -5,6 +5,8 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,7 +20,17 @@ public class CustomGuiListener implements Listener {
 
   @EventHandler
   public void onCustomGuiInteract(@NotNull InventoryClickEvent event) {
-	if (event.getInventory().getContents()[0].getItemMeta().getPersistentDataContainer().get(new NamespacedKey(plugin, "is_custom_gui"), PersistentDataType.BOOLEAN) != null) {
+	ItemStack stack = event.getInventory().getContents()[0];
+	if (stack == null) {
+	  return;
+	}
+
+	ItemMeta meta = stack.getItemMeta();
+	if (meta == null) {
+	  return;
+	}
+
+	if (meta.getPersistentDataContainer().get(new NamespacedKey(plugin, "is_custom_gui"), PersistentDataType.BOOLEAN) != null) {
 	  event.setCancelled(true);
 	}
   }
